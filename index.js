0903c86cf13dfb9193ce268ed2558bb3092ee0e1
Alexander <96749659+Nazeofel@users.noreply.github.com>
2023-06-15 12:28:09 +0200

    now work

---
+++ index.js
@@ -0,0 +1,75 @@
+const PORT = process.env.PORT || 8080;
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const app = express();
+
+const newsArticles = [
+  {
+    name: "techcrunch",
+    address: "https://techcrunch.com/category/cryptocurrency/",
+    base: "", // in case the URL is not completed
+  },
+];
+
+// prettier-ignore
+const keywords = 'a:contains("crypto"), a:contains("web3"), a:contains("blockchain"), a:contains("bitcoin"), a:contains("ethereum"), a:contains("binance"), a:contains("coinbase"), a:contains("sec"), a:contains("xrp"), a:contains("ripple"), a:contains("solana"), a:contains("coin")';
+
+let articles = [];
+
+// Function to fetch articles and populate the articles array
+const fetchArticles = async () => {
+  newsArticles.forEach((newsArticle) => {
+    axios
+      .get(newsArticle.address)
+      .then((response) => {
+        console.log(response);
+        const html = response.data;
+        const $ = cheerio.load(html);
+        let imageUrl;
+
+        $(keywords, html).each(function (index) {
+          const title = $(this).text();
+          const url = $(this).attr("href");
+
+          if (
+            newsArticle.address ===
+            "https://techcrunch.com/category/cryptocurrency/"
+          ) {
+            const articleElements = $(".post-block");
+            const articleElement = articleElements.eq(index);
+            const img = articleElement.find("footer img");
+            imageUrl = img.attr("src");
+          }
+
+          articles.push({
+            title,
+            imageUrl,
+            url: newsArticle.base + url,
+            source: newsArticle.name,
+          });
+        });
+      })
+      .catch((error) => console.log(error));
+  });
+};
+// Call the fetchArticles function to populate the articles array
+
+app.use((req, res, next) => {
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  next();
+});
+
+app.get("/api/news", async (req, res, next) => {
+  await fetchArticles();
+
+  return res.json(articles);
+});
+
+app.listen(PORT, () => {
+  console.log("Server running...");
+});
+
+module.exports = app;
